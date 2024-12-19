c1287c4d5af865f33bb34d2d9303895528e0f36d
aprendiz-funcional <aprendiz.funcional@gruporeditos.com>
2024-12-19 15:25:20 -0500

    creacion de funciones para cruzar los datos

--- Datos/cruce_info.py
+++ Datos/cruce_info.py
@@ -1,20 +1,24 @@
 import win32com.client as win32
 import pandas as pd
 import os
+from datetime import datetime
 from openpyxl import load_workbook
+
 from Datos.pasos import Pasos
+
 
 class GuardarDatos(Pasos):
 
     def __init__(self):
         #Inicializa una instancia de Excel para trabajar con archivos de Excel.
-
-        self.excel = win32.gencache.EnsureDispatch('Excel.Application')
-        self.excel.Visible = False  # Cambiar a True para ver el proceso en Excel.
+        
+        #self.excel = win32.gencache.EnsureDispatch('Excel.Application')
+        #self.excel.Visible = False  # Cambiar a True para ver el proceso en Excel.
         self.path_ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.path_Reportes= os.path.join(self.path_,'Reportes')
         self.path_ResultadoArchivo= os.path.join(self.path_,'Resultado', 'Adulto_mayor.xlsx')
 
-    def cargar_archivos(self):
+    """ def cargar_archivos(self):
         #Carga los archivos necesarios para el procesamiento.
         columnas_Pagos=['Fecha','Ciudad','Zona','Cédula Cajero/Vendedor','Cajero/Vendedor',
             'Canal', 'Reclama titular?', 'Empresa', 'Producto','Cod. Oficina', 'Oficina',
@@ -35,9 +39,9 @@ class GuardarDatos(Pasos):
         self.reporte_pagos.columns = self.reporte_pagos.columns.str.strip()
         self.reporte_pagos[['Fecha', 'Hora']] = self.reporte_pagos['Fecha'].str.split(' ', expand=True)
         print("Reporte Pagos después de separar Fecha y Hora:",self.reporte_pagos)
-        print(self.reporte_pagos[['Fecha', 'Hora']].head())
+        print(self.reporte_pagos[['Fecha', 'Hora']].head()) """
 
-    def procesar_datos(self):
+    """ def procesar_datos(self):
         #Procesa los datos cargados, separa fecha y hora, y selecciona las columnas de interés.
         # Separar la columna FechaHora en Fecha y Hora
 
@@ -54,53 +58,179 @@ class GuardarDatos(Pasos):
             'AF': 'Titular Comfama'}
         # Mapeo de columnas de reporte_usuarios
         self.mapeo_columnas_usuarios = {
-            'G':'Cargo','AG':'Estado',}
+            'G':'Cargo','AG':'Estado',} """
 
-    def convertir_columna_a_indice(self, columna):
+    """ def convertir_columna_a_indice(self, columna):
         #Convierte una columna en formato letra (A, B, AA) a un índice numérico.
         resultado = 0
         for char in columna:
             resultado = resultado * 26 + (ord(char.upper()) - ord('A') + 1)
-        return resultado
+        return resultado """
     
-    def procesar_excel(self):
+    def __lista_archivo_final(self):
         print(self.path_ResultadoArchivo)
         df = pd.read_excel(self.path_ResultadoArchivo)
         
-        titulos_df = pd.DataFrame({'Titulos': df.columns})
-        print(type(titulos_df))
-        print(titulos_df)
+         # Crear una lista con los nombres de las columnas
+        titulos_lista = list(df.columns)
+        print("Lista de títulos:", titulos_lista)
+        return titulos_lista
 
-        # Seleccionar solo las columnas que existen en df_destino 
-        columnas_comunes = [col for col in titulos_df['Titulos']if col in self.reporte_pagos.columns]
-        df=df[columnas_comunes] 
-        #titulos_df = self.reporte_pagos[columnas_comunes]        
-        print(df)
+    def __cargar_archivos_usuarios(self):
+        path_reporte_usuarios = os.path.join(self.path_Reportes, 'reporteUsuarios.xlsx' )
+        path_reporte_usuarios_m = os.path.join(self.path_Reportes, 'reporteUsuarios_modificado.xlsx' )
+        #Se carga el archivo de usuarios
+        wb_reporte_usuarios = load_workbook(path_reporte_usuarios)
+        hoja = wb_reporte_usuarios.active
+        # Lista de columnas a eliminar
+        columnas_a_eliminar = [2, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+        # Eliminar las columnas desde el final hacia el principio para evitar problemas con el cambio de índice
+        for col in sorted(columnas_a_eliminar, reverse=True):
+            print(f"Eliminando columna usuarios: {col}")
+            hoja.delete_cols(col)
+        #wb_reporte_usuarios.save(path_reporte_usuarios_m)
+        return hoja
 
-    def cargar_archivos1(self):
-        #Carga los archivos necesarios para el procesamiento.
-        columnas_Pagos=['Fecha','Ciudad','Zona','Cédula Cajero/Vendedor','Cajero/Vendedor',
-            'Canal', 'Reclama titular?', 'Empresa', 'Producto','Cod. Oficina', 'Oficina',
-            'Cod. Sitio ','Sitio de venta','Tipo Doc. Titular', 'Identificación Titular',  'Titular',
-            'Tipo Doc. Autorizado', 'Identificación Autorizado', 'Autorizado','Periodo', 'Valor Reportado', 
-            'Valor Pagado', 'Valor Redondeo', 'Grupo Pago','Tipo Pago','Tipo Subsidio', 'Titular Comfama', 
-            'Titular Comfama']
-        self.reporte_pagos = None
-        self.reporte_pagos = load_workbook('reportes/reportePagos.xlsx')
-        print(self.reporte_pagos)
+    def __cargar_archivos_pagos(self):
+        path_reporte_pagos = os.path.join(self.path_Reportes,'reportePagos.xlsx' )
+        path_reporte_pagos_m = os.path.join(self.path_Reportes,'reportePagos_modificado.xlsx' )
+        #Se carga el archivo de pagos
+        wb_reporte_pagos = load_workbook(path_reporte_pagos)
+        # Selecciona la hoja activa
+        hoja = wb_reporte_pagos.active
+        #Se crea lista para eliminar columnas 
+        columnas_a_eliminar =[2,4,8]
+        # Eliminar las columnas desde el final hacia el principio para evitar problemas con el cambio de índice
+        for col in sorted(columnas_a_eliminar, reverse=True):
+            print(f"Eliminando columna pagos: {col}")
+            hoja.delete_cols(col)
+        # Insertar una nueva columna para la hora
+        # Inserta una nueva columna en la posición 2 (después de la columna A)
+        hoja.insert_cols(2)  
+        hoja.cell(row=1, column=2, value="Hora")
+        # Procesar la columna A (fecha y hora) y dividir los datos
+        # Ajusta las filas según tu archivo
+        for fila in hoja.iter_rows(min_row=2, max_row=hoja.max_row, min_col=1, max_col=1):
+            # Celda de la columna A (suponiendo que ahí están las fechas y horas) 
+            celda = fila[0] 
+            # Verificar que la celda no esté vacía
+            if celda.value:  
+                try:
+                    # Convertir el valor a datetime
+                    fecha_hora = datetime.strptime(celda.value, '%d/%m/%Y %H:%M:%S')
+                    fecha = fecha_hora.date()
+                    hora = fecha_hora.time()
+                    # Sobreescribir la columna A con solo la fecha
+                     # Columna A para la fecha
+                    hoja.cell(row=celda.row, column=1, value=str(fecha)) 
+                    # Escribir la hora en la nueva columna (columna B)
+                    hoja.cell(row=celda.row, column=2, value=str(hora))  # Columna B para la hora
+                except Exception as e:
+                    print(f"Error al convertir la celda en la fila {celda.row}: {celda.value} {e}")
 
-        columnas_Usuarios=['Identificacion','Cargo','Estado']
-        self.reporte_usuarios = None
-        self.reporte_usuarios = load_workbook('reportes/reporteUsuarios.xlsx')
-        print(self.reporte_usuarios)
+        # Guardar los cambios en un nuevo archivo
+        #wb_reporte_pagos.save(path_reporte_pagos_m)
+        return hoja
 
-         #Procesa los datos cargados, separa fecha y hora, y selecciona las columnas de interés.
-        # Separar la columna FechaHora en Fecha y Hora
+    def cruzar_archivos(self):
+        hora_inicio = datetime.now()
+        print()
+        hoja_reporte_pagos = self.__cargar_archivos_pagos()
+        hoja_reporte_usuarios = self.__cargar_archivos_usuarios()
+        titulos_lista = self.__lista_archivo_final()
 
-        """ self.reporte_pagos.columns = self.reporte_pagos.columns.str.strip()
-        self.reporte_pagos[['Fecha', 'Hora']] = self.reporte_pagos['Fecha'].str.split(' ', expand=True) """
+        # Crear un nuevo Workbook para el resultado
+        wb_resultado = load_workbook(self.path_ResultadoArchivo)
+        hoja_resultado = wb_resultado['DATA']
 
-    def guardar_en_excel(self, hoja):
+        #hoja_pagos = wb_reporte_pagos.active
+
+        # Transferir los datos de wb_reporte_pagos al nuevo workbook
+        for row_num, fila in enumerate(hoja_reporte_pagos.iter_rows(min_row=2, values_only=True), 2):
+            # Llenar las columnas según los títulos de titulos_lista
+            identificacion = fila[19]
+            print(f"identificacion a llenar: {identificacion}")
+            cargo_autorizado = self.__buscar_usuarios_autorizado(hoja_reporte_usuarios, identificacion)
+            i = 0
+            
+            for col_num, titulo in enumerate(titulos_lista):
+            
+                for cell in hoja_reporte_pagos[1]:  # Suponiendo que los títulos están en la fila 1
+                    if titulo != cell.value:
+                        continue
+                
+                #if titulo == hoja_reporte_pagos.columns[titulo]  # Asegurarse de que el título esté presente en wb_reporte_pagos
+                    
+                    if titulo == "Cargo":
+                        identificacion = fila[4]
+                        cargo = self.__buscar_usuarios_cajero(hoja_reporte_usuarios, identificacion)
+                        if cargo:
+                            print(f"Celda a llenar: {titulo} valor: '{cargo}'")
+                            hoja_resultado.cell(row=row_num, column=col_num, value=cargo)
+                        else:
+                            continue
+
+                    elif titulo == "Cargo autorizado" or titulo == "Oficina" or titulo == "Estado":
+                        
+                        if cargo_autorizado is not None:
+                            hoja_resultado.cell(row=row_num, column=col_num, value=cargo_autorizado[i])
+                            print(f"Celda a llenar: {titulo} valor: '{cargo_autorizado[i]}'")
+                        else:
+                            hoja_resultado.cell(row=row_num, column=col_num, value='')
+                            print(f"Celda a llenar: {titulo} valor: 'vacio'")
+                        i += 1
+                        continue
+
+                    col_index = hoja_reporte_pagos.columns.index(col_num) + 1
+                    # Copiar los datos de la columna correspondiente de wb_reporte_pagos
+                    hoja_resultado.cell(row=row_num, column=col_num, value=fila[col_index - 1])
+                    print(f"Celda a llenar: {titulo} valor: {fila[col_index - 1]}")
+
+        # Guardar el nuevo workbook con los datos combinados
+        wb_resultado.save(self.path_ResultadoArchivo)
+        hora_final = datetime.now()
+        print(f"hora inicio {hora_inicio}. hora final {hora_final}")
+
+    def __buscar_usuarios_cajero(self, hoja_reporte_usuarios, num_identificacion):
+
+        # Obtener los datos del único usuario
+        for fila in hoja_reporte_usuarios.iter_rows(min_row=2, values_only=True):
+            identificacion = fila[3]  # Suponiendo que la identificación está en la cuarta columna (índice 3)
+            cargo = fila[2]  # Suponiendo que el cargo está en la tercera columna (índice 2)
+            print(f"Identificacion busacada: {identificacion}")        
+            if identificacion == num_identificacion:  # Comparar con el número de identificación que deseas buscar
+                print(f"Coicidencia encontra cajero: identicacion: '{num_identificacion}', Identificacion cajero: {identificacion} ")
+                cargo_cajero = cargo
+                return cargo_cajero
+        return None
+
+    def __buscar_usuarios_autorizado(self, hoja_reporte_usuarios, num_identificacion):
+        # Crear una lista para almacenar la información del único usuario
+        usuario_lista = []
+
+        # Obtener los datos del único usuario
+        for fila in hoja_reporte_usuarios.iter_rows(min_row=2, values_only=True):
+            identificacion = fila[3]  # Suponiendo que la identificación está en la cuarta columna (índice 3)
+            cargo = fila[2]  # Suponiendo que el cargo está en la tercera columna (índice 2)
+            oficina = fila[1]  # Suponiendo que la oficina está en la segunda columna (índice 1)
+            estado = fila[0]  # Suponiendo que el estado está en la primera columna (índice 0)
+           
+            if identificacion == num_identificacion:  # Comparar con el número de identificación que deseas buscar
+                print(f"Coicidencia encontra para la identicacion: {num_identificacion}, Identificacion autorizada: {identificacion} ")
+                usuario_lista = [cargo, oficina, estado,]
+                return usuario_lista
+        return None
+
+        # Si el usuario existe, puedes acceder a los valores como:
+        if usuario_lista:
+            print(f"Identificación: {usuario_lista[0]}")
+            print(f"Cargo: {usuario_lista[1]}")
+            print(f"Estado: {usuario_lista[2]}")
+            print(f"Oficina: {usuario_lista[3]}")
+        else:
+            print("Usuario no encontrado")
+
+    """ def guardar_en_excel(self, hoja):
         #Escribe los datos procesados en el archivo Excel en la hoja especificada.
         wb = self.excel.Workbooks.Open(self.path_ResultadoArchivo)
         try:
@@ -140,5 +270,5 @@ class GuardarDatos(Pasos):
         wb.Close()
 
     def cerrar_excel(self):
-        """Cierra la instancia de Excel."""
-        self.excel.Quit()
+        Cierra la instancia de Excel.
+        self.excel.Quit() """
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ guardador = GuardarDatos()
 # Verificar o crear el archivo de destino
 
 # Cargar, procesar y guardar los datos en el archivo Excel
-guardador.cargar_archivos()
-guardador.cargar_archivos1()
+#guardador.cargar_archivos()
+#guardador.cargar_archivos_usuarios()
+guardador.cruzar_archivos()
 #guardador.procesar_excel()
-""" guardador.procesar_datos()
-guardador.guardar_en_excel('DATA')
-guardador.cerrar_excel() """
+#guardador.procesar_datos()
+#guardador.guardar_en_excel('DATA')
+#guardador.cerrar_excel()
 
 print("Datos guardados correctamente.")
